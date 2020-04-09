0b66ab6d1b7d610af369618c1d52dccce9c7736b
David Mendoza <d.mendoza99@unitec.edu>
2020-04-09 16:36:34 -0600

    added the new items for the component

--- scripts/RedTank.lua
+++ scripts/RedTank.lua
@@ -1,10 +1,15 @@
 Tank = {
     graphicComponent = {
         spriteName = "RedTank",
-        spriteFilepath = "assets/RedTank.png",
+        spriteFilepath = "assets/Sprites/RedTank.png",
         scale = {
             width = 1,
             height = 1
+        },
+        origin = {
+            x = 64,
+            y = 64
         }
-    }
+    },
+    logicalComponent = {}
 }
