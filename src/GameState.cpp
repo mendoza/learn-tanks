b3dc497d776061f6e14b26ceed8df8f9d4e63cfd
David Mendoza <d.mendoza99@unitec.edu>
2020-04-09 16:36:25 -0600

    _notation

--- src/GameState.cpp
+++ src/GameState.cpp
@@ -4,29 +4,30 @@
 
 namespace Skeleton {
 GameState::GameState(gameDataRef data) : systems(tanks) {
-  this->_data = data;
+  this->data = data;
   this->script.script_file("scripts/RedTank.lua");
   sol::table player = this->script["Tank"];
   this->redTank = this->tanks.create();
-  this->redTank.add<GraphicComponent>(this->_data, player["graphicComponent"]);
-  this->systems.add<drawGraphicSystem>(this->_data);
+  this->redTank.add<GraphicComponent>(this->data, player["graphicComponent"]);
+  this->systems.add<drawGraphicSystem>(this->data);
 }
-// GameState::~GameState() { }
+
 void GameState::init() {}
 
 void GameState::handleInput() {
   sf::Event event;
-  while (this->_data->window.pollEvent(event)) {
+  while (this->data->window.pollEvent(event)) {
     if (sf::Event::Closed == event.type) {
-      this->_data->window.close();
+      this->data->window.close();
     }
   }
 }
 
 void GameState::update(float dt) {
-  this->_data->window.clear(sf::Color(125, 125, 125));
+  this->data->window.clear(sf::Color(125, 125, 125));
   this->systems.update(dt);
+  this->redTank.get<GraphicComponent>().sprite.rotate(45);
 }
 
-void GameState::draw() { this->_data->window.display(); }
+void GameState::draw() { this->data->window.display(); }
 } // namespace Skeleton
