8f7ed644e37d949357d925d3526aa7ef8dee4dd6
David Mendoza <d.mendoza99@unitec.edu>
2020-04-11 22:29:40 -0600

    delete some comments and added the movable system (might merge it with graphicSystem)

--- include/Systems.hpp
+++ include/Systems.hpp
@@ -7,7 +7,6 @@ class GraphicSystem : public ecs::System {
 	Skeleton::GameDataRef Data;
 	GraphicSystem(Skeleton::GameDataRef Data) : Data(Data) {}
 	void update(float time) override {
-		// Get the entity manager using entities() function
 		for (auto entity : entities().with<GraphicComponent>()) {
 			entity.get<GraphicComponent>().update(time);
 			this->Data->Window.draw(entity.get<GraphicComponent>().Sprite);
@@ -20,9 +19,27 @@ class LogicSystem : public ecs::System {
 	LogicSystem() {}
 
 	void update(float time) override {
-		// Get the entity manager using entities() function
 		for (auto entity : entities().with<LogicComponent>()) {
 			entity.get<LogicComponent>().update(time);
+		}
+	}
+};
+
+class MovableSystem : public ecs::System {
+  public:
+	MovableSystem() {}
+
+	void update(float time) override {
+		for (auto entity :
+			 entities().with<MovableComponent, GraphicComponent>()) {
+			entity.get<MovableComponent>().update(time);
+			sf::Vector2f Direction;
+			float angle = entity.get<GraphicComponent>().Sprite.getRotation();
+			Direction.x = std::cos(angle);
+			Direction.y = std::sin(angle);
+
+			entity.get<GraphicComponent>().Position +=
+				Direction * entity.get<MovableComponent>().Speed;
 		}
 	}
 };
