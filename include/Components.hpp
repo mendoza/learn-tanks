a59e300663811a264dd6d5eee1ae317482a08dd6
David Mendoza <d.mendoza99@unitec.edu>
2020-04-11 22:27:43 -0600

    added position to the componenets

--- include/Components.hpp
+++ include/Components.hpp
@@ -13,6 +13,7 @@ class Component {
 class GraphicComponent : public Component {
   public:
 	sf::Sprite Sprite;
+	sf::Vector2f Position;
 	sf::Vector2u ImageCount;
 	sf::Vector2u CurrentImage;
 	std::string Name;
@@ -26,11 +27,28 @@ class GraphicComponent : public Component {
 		this->Name = GC["spriteName"];
 		Data->Assets.loadTexture(this->Name, GC["spriteFilepath"]);
 		sf::Texture &text = Data->Assets.getTexture(this->Name);
-		sf::Vector2f scale(GC["scale"]["width"], GC["scale"]["height"]);
-		sf::Vector2f origin(GC["origin"]["x"], GC["origin"]["y"]);
-		this->Sprite.setOrigin(origin);
 		this->Sprite.setTexture(text);
-		this->Sprite.setScale(scale);
+
+		if (GC["origin"] != sol::nil) {
+			sf::Vector2f origin(GC["origin"]["x"], GC["origin"]["y"]);
+			this->Sprite.setOrigin(origin);
+		}
+
+		if (GC["scale"] != sol::nil) {
+			sf::Vector2f scale(GC["scale"]["width"], GC["scale"]["height"]);
+			this->Sprite.setScale(scale);
+		}
+		if (GC["position"] != sol::nil) {
+			this->Position =
+				sf::Vector2f(GC["position"]["x"], GC["position"]["y"]);
+			this->Sprite.setPosition(Position);
+		}
+		// Maybe add it later...
+		// if (GC["rotation"] != sol::nil) {
+		// 	sf::Vector2f vector(GC["rotation"]["x"], GC["rotation"]["y"]);
+		// 	float angle = std::atan(vector.y / vector.x) * 180 / M_PI;
+		// 	this->Sprite.setRotation(angle);
+		// }
 	}
 
 	void update(float dt) override {
@@ -47,19 +65,30 @@ class GraphicComponent : public Component {
 			UvRect.top = CurrentImage.y * UvRect.height;
 			this->Sprite.setTextureRect(this->UvRect);
 		}
+		this->Sprite.setPosition(Position);
 	}
 };
 
 class LogicComponent : public Component {
   public:
 	sol::state L;
-	sol::function updates;
+	sol::function ScriptUpdate;
 	LogicComponent(std::string Path) {
 		L.script_file(Path);
 		L.open_libraries();
-		this->updates = L["Tank"]["logicalComponent"]["update"];
+		this->ScriptUpdate = L["Tank"]["logicalComponent"]["update"];
 	}
-	void update(float dt) override { this->updates(); }
+	void update(float dt) override { this->ScriptUpdate(); }
+
+	void draw() override {}
+};
+
+class MovableComponent : public Component {
+  public:
+	float Speed;
+	MovableComponent(float Speed) { this->Speed = Speed; }
+
+	void update(float dt) override {}
 
 	void draw() override {}
 };
